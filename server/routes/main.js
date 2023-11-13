cf64a5834cf04cd843cb389feb97f6520b935044
Rizjami Putera <rizjamiputera@gmail.com>
2023-11-13 22:15:56 +0700

    add pagination to get santri

--- server/routes/main.js
+++ server/routes/main.js
@@ -11,8 +11,33 @@ router.get('/', async (req, res) => {
 
 // GET SANTRI
 router.get('/santri', async (req, res) => {
-    const santries = await Santri.find();
-    res.json(santries);
+    const perPage = parseInt(req.query.perpage) || 10;
+    const page = parseInt(req.query.page) || 1;
+
+    // const santries = await Santri.find();
+    const data = await Santri.aggregate([ { $sort: { firstname: 1 }} ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+    const count = await Santri.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    const from = data.length ? perPage * page - perPage + 1 : 0;
+    const to = data.length ? from + data.length - 1 : 0;
+    const maxPage = Math.ceil(count / perPage);
+
+    res.json({
+        data,
+        currentPage: page,
+        prevPage: page != 1 ? page - 1 : null,
+        nextPage: hasNextPage ? nextPage : null,
+        maxPage,
+        from,
+        to,
+        total: count
+    });
 });
 
 module.exports = router;
